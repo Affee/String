0dba8c67fb7b8618e228b3e7a89a422e95b43d79
Affee <304442951@qq.com>
2018-05-24 18:07:02 +0800

    puts() 函数值显示字符串，而且自动在显示字符串末尾加上换行符

--- 11章 String/main.c
+++ 11章 String/main.c
@@ -5,11 +5,23 @@
 //  Created by Affee on 2018/5/23.
 //  Copyright © 2018年 affee. All rights reserved.
 //
+//字符串是依控制符(\O)结尾的char类型数组。
 
 #include <stdio.h>
+#define MSG "I am a symbolic string constant"
+#define MAXLENGTH 81
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    char words[MAXLENGTH]  = "I am a string int an array";
+    const char *pt1 = "Something is pointing at me.";
+    puts("Here are some strings:");
+    puts(MSG);
+    puts(words);
+    puts(pt1);
+    words[8] = 'p';
+    puts(words);
     printf("Hello, World!\n");
+    
     return 0;
 }
+
